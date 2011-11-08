f2b13ef5e0e3d2922c17065b39010ae9ca0e117d
abbradar <nikoamia@gmail.com>
2011-11-08 22:54:09 +0400

    Cleaned result

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 1 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Tue Nov 8 01:42:31 CET 2011"
-#define LINUX_COMPILE_TIME "01:42:31"
-#define LINUX_COMPILE_BY "mischu"
-#define LINUX_COMPILE_HOST "breath"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-51) "
